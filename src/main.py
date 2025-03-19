9feeca10d22ccaf97ee7255e396827b0e35fb16e
Syed Tathahir Uddin <syedtathahiruddin@gmail.com>
2025-03-20 01:03:26 +0530

    changed port

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     gui_multi_pages.run(
         use_reloader=True,
         title="Olympic medals 🥇",
-        port=2452,
+        port=10000,
         dark_mode=False,
     )
 
